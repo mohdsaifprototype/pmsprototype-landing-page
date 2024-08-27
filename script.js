7297b12ac3cfaeebfd8f213ba929f2e3cea661de
Mohd Saif <mohdsaifsiddique.feaindia@gmail.com>
2024-08-27 12:20:05 +0530

    Website v1.0

---
+++ script.js
@@ -0,0 +1,6 @@
+// const colorPallete = ["#ffe98a", "#fff3bd", "#ffdf57", "#411271", "#0e0419", "#280b45"];
+
+// /* 
+//   Write a program that randomly chooses a color from the colorPallete and sets to :root css variables in css file.
+// */
+
